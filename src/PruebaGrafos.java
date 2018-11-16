f944954440197b0e8a72bfac875029aadc3a04fa
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-15 20:20:07 -0600

    Se agrego la clase GrafoAdcia en PruebaGrafos.java

--- src/PruebaGrafos.java
+++ src/PruebaGrafos.java
@@ -300,6 +300,29 @@ class Arco{
     	return destino == a.destino;
     }
 } //Class Arco
+
+class GrafoAdcia{
+	int numVerts;
+    static int maxVerts = 20;
+    Vertice [] tablAdc;
+    public GrafoAdcia(int mx){
+    	tablAdc = new Vertice[mx];
+        numVerts = 0;
+        maxVerts = mx;
+    }
+    public int numVertice(String vs) {
+        Vertice v = new Vertice(vs);
+        boolean encontrado = false;
+        int i = 0;
+        for (; (i < numVerts) && !encontrado;){
+       	 encontrado = tablAdc[i].equals(v);
+       	 if (!encontrado) 
+       		 i++ ;
+       	 }
+        return (i < numVerts) ? i : -1 ;
+   }
+    
+} //Clase GrafoAdcia
 public class PruebaGrafos {
 
 	public static void main(String[] args) {
