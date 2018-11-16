e3d2c3840cccfcf5e998ef1a8dd47ded881d1d75
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-15 20:17:20 -0600

    Se agrego la clase PilaLista a PruebaGrafos.java

--- src/PruebaGrafos.java
+++ src/PruebaGrafos.java
@@ -245,6 +245,42 @@ class ColaLista {
     	return (frente == null);
     	}
 }//class ColaLista
+
+class PilaLista { 
+	private NodoPila cima;
+	public PilaLista(){
+		cima = null;
+    }
+	public boolean pilaVacia(){
+		return cima == null;
+	}
+	public void insertar(Object elemento){
+		NodoPila nuevo;
+	    nuevo = new NodoPila(elemento);
+	    nuevo.siguiente = cima;
+	    cima = nuevo;
+	}
+	public Object quitar() throws Exception{
+		if (pilaVacia())
+			throw new Exception ("Pila vacía, no se puede extraer.");
+		Object aux = cima.elemento;
+		cima = cima.siguiente;
+		return aux;
+	}
+	public Object cimaPila() throws Exception{
+		if (pilaVacia())
+			throw new Exception ("Pila vacía, no se puede leer cima.");
+		return cima.elemento;
+	}
+	public void limpiarPila(){
+		NodoPila t;
+		while(!pilaVacia()){
+			t = cima;
+			cima = cima.siguiente;
+			t.siguiente = null;
+	}
+	}
+}//Class PilaLista
 public class PruebaGrafos {
 
 	public static void main(String[] args) {
